b3947528f3dc6d60c5ecad03c5c9ef2f0e9c90f2
Yair Sadan <yairsadan1@gmail.com>
2024-01-14 19:40:54 +0200

    Add Users Controller

---
+++ Todos.API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todos.API.Data;
+
+namespace Todos.API.Controllers;
+// https://localhost:portnumber/api/users
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase {
+    private readonly TodosDbContext dbContext;
+    public UsersController(TodosDbContext dbContext) {
+        this.dbContext = dbContext;
+    }
+
+    // GET ALL USERS
+    // GET: https://localhost:portnumber/api/users
+    [HttpGet]
+    public IActionResult GetAll() {
+        var users = dbContext.Users.ToList();
+        return Ok(users);
+    }
+
+    // GET USER BY ID
+    // GET: https://localhost:portnumber/api/users/{id}
+    [HttpGet]
+    [Route("{id:Guid}")]
+    public IActionResult GetById([FromRoute] Guid id) {
+        var user = dbContext.Users.FirstOrDefault(x => x.Id == id);
+        if (user == null)
+            return NotFound();
+        return Ok(user);
+    }
+}
