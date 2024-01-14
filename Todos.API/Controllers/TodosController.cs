386b72ae3f386103294f0bf8ad184248c14dc8b6
Yair Sadan <yairsadan1@gmail.com>
2024-01-14 19:40:30 +0200

    Fix bugs, Add a connection string

--- Todos.API/Controllers/TodosController.cs
+++ Todos.API/Controllers/TodosController.cs
@@ -9,7 +9,12 @@ namespace Todos.API.Controllers {
         //https://localhost:portnumber/api/todos
         [HttpGet]
         public IActionResult GetAllTodos() {
-
+            string[] todos = new string[] {
+                "Todo 1",
+                "Todo 2",
+                "Todo 3"
+            };
+            return Ok(todos);
         }
     }
 }
