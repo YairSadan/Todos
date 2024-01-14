1163a692746974c548ed25cbc07fd026eee14ef5
Yair Sadan <yairsadan1@gmail.com>
2024-01-14 18:32:30 +0200

    Revert "Add project files."
    This reverts commit 0dd5ece32af8c79f8d9d7c6d6a582d848edd6279.

--- Todos.API/Models/Domain/User.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Todos.API.Models.Domain {
-    public class User {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string? ImageUrl { get; set; }
-    }
-}
