f2d04f4ee755defabda35887713941f40d4ebb15
Yair Sadan <yairsadan1@gmail.com>
2024-01-14 18:42:09 +0200

    Reapply "Add project files."
    This reverts commit 1163a692746974c548ed25cbc07fd026eee14ef5.

---
+++ Todos.API/Controllers/TodosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todos.API.Controllers {
+    // https://localhost:portnumber/api/todos
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase {
+        //https://localhost:portnumber/api/todos
+        [HttpGet]
+        public IActionResult GetAllTodos() {
+
+        }
+    }
+}
