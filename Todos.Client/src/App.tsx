bd9646dc4e266e1538d444e168d1d0e02f40650b
Yair Sadan <yairsadan1@gmail.com>
2024-01-16 19:46:13 +0200

    Add Dark mode compatbility and mode-toggle

--- Todos.Client/src/App.tsx
+++ Todos.Client/src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import { ModeToggle } from './components/mode-toggle';
+import { ThemeProvider } from './components/theme-provider';
 function App() {
-  return <div>Hello world</div>;
-
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
+      <div>hello</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
