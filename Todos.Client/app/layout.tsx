fb5d33fb9338c313b12610785ab7eb4d9bd1af0a
Yair Sadan <yairsadan1@gmail.com>
2024-01-17 13:40:38 +0200

    Add dark mode compatbility

--- Todos.Client/app/layout.tsx
+++ Todos.Client/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 import { cn } from '../lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
+import { ModeToggle } from '@/components/mode-toggle';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -17,7 +19,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <ModeToggle />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
