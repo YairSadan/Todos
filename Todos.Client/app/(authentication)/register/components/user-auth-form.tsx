c0d0597eeb0506c0d19e146ec077682ce3605319
Yair Sadan <yairsadan1@gmail.com>
2024-01-21 21:07:59 +0200

    added register page and dark mode compatability to login page

---
+++ Todos.Client/app/(authentication)/register/components/user-auth-form.tsx
@@ -0,0 +1,141 @@
+'use client';
+import * as React from 'react';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { ZodStringCheck, z } from 'zod';
+import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const registerSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8)
+    .refine((data) => !data.match(/[A-Z]/), {
+      message: 'Password must contain at least one uppercase letter',
+    })
+});
+
+const registerFetch = async (email: string, password: string) => {
+  const res = await fetch(`http://localhost:5160/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+  const result = await res.json();
+  if (result.status === 200) return true;
+  else if (result.status === 400) {
+    if (result.errors.DuplicateUserName) throw new Error('User with this email already exists');
+    throw new Error('Check your email and password and try again');
+  }
+  throw new Error('Something went wrong');
+};
+
+type RegisterSchemaType = z.infer<typeof registerSchema>;
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterSchemaType>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = (data: RegisterSchemaType) => {
+    setIsLoading(true);
+    try {
+      registerFetch(data.email, data.password);
+      router.push('/login');
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className={cn('grid gap-6', className)} {...props}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              {...register('email')}
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Password
+            </Label>
+            <Input
+              {...register('password')}
+              id="password"
+              placeholder="SomePassword123!"
+              type="password"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="r-2 h-4 w-4 animate-spin">
+                <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+              </svg>
+            )}
+            Sign In with Email and Password
+          </Button>
+          <div className="flex flex-col h-8 space-y-1 my-1" aria-live="polite" aria-atomic="true">
+            {errors.email && (
+              <div className="flex space-x-1">
+                <ExclamationCircleIcon className="h-5 w-5 text-warning" />
+                <p className="text-sm text-warning">{errors.email?.message}</p>
+              </div>
+            )}
+            {errors.password && (
+              <div className="flex space-x-1">
+                <ExclamationCircleIcon className="h-5 w-5 text-warning" />
+                <p className="text-sm text-warning">{errors.password?.message}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
--- Todos.Client/components/login-form.tsx
+++ Todos.Client/components/login-form.tsx
@@ -7,6 +7,7 @@ import { Button } from './ui/button';
 import { AtSymbolIcon, KeyIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { useRouter } from 'next/navigation';
+import { Input } from './ui/input';
 
 const signInScema = z.object({
   email: z.string().email(),
@@ -24,7 +25,6 @@ const LoginForm: React.FC = () => {
     resolver: zodResolver(signInScema),
   });
   const router = useRouter();
-
   const login = async (email: string, password: string) => {
     const res = await fetch(`http://localhost:5160/login`, {
       method: 'POST',
@@ -39,7 +39,7 @@ const LoginForm: React.FC = () => {
     });
     const data = await res.json();
     if (res.status === 200) {
-      const { accessToken, refreshToken} = data;
+      const { accessToken, refreshToken } = data;
       sessionStorage.setItem('accessToken', accessToken);
       sessionStorage.setItem('refreshToken', refreshToken);
       router.push('/todos');
@@ -47,61 +47,62 @@ const LoginForm: React.FC = () => {
     // TODO add a wrong credentials modal
   };
 
+
   const onSubmit = (data: SignInSchemaType) => {
     login(data.email, data.password);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+      <div className="flex-1 rounded-lg card-foreground border px-6 pb-4 pt-8">
         <h1 className="mb-3 text-2xl">Please log in to continue.</h1>
         <div className="w-full">
           <div>
-            <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="email">
+            <label className="mb-3 mt-5 block text-xs font-medium" htmlFor="email">
               Email
             </label>
             <div className="relative">
-              <input
+              <Input
                 {...register('email')}
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full rounded-md border py-[9px] pl-10 text-sm outline-2"
                 id="email"
                 type="email"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="name@example.com"
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2" />
             </div>
           </div>
           <div className="mt-4">
-            <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="password">
+            <label className="mb-3 mt-5 block text-xs font-medium" htmlFor="password">
               Password
             </label>
             <div className="relative">
-              <input
+              <Input
                 {...register('password')}
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full rounded-md border py-[9px] pl-10 text-sm outline-2"
                 id="password"
                 type="password"
                 name="password"
-                placeholder="Enter password"
+                placeholder="SomePassword123!"
               />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2" />
             </div>
           </div>
         </div>
         <LoginButton />
-        <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
+        <div className="flex flex-col h-8 space-y-1 my-1" aria-live="polite" aria-atomic="true">
           {errors.email && (
-            <>
-              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errors.email?.message}</p>
-            </>
+            <div className='flex space-x-1'>
+              <ExclamationCircleIcon className="h-5 w-5 text-warning" />
+              <p className="text-sm text-warning">{errors.email?.message}</p>
+            </div>
           )}
           {errors.password && (
-            <>
-              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errors.password?.message}</p>
-            </>
+            <div className='flex space-x-1'>
+              <ExclamationCircleIcon className="h-5 w-5 text-warning" />
+              <p className="text-sm text-warning">{errors.password?.message}</p>
+            </div>
           )}
         </div>
       </div>
@@ -112,7 +113,7 @@ const LoginForm: React.FC = () => {
 function LoginButton() {
   return (
     <Button className="mt-4 w-full">
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      Log in <ArrowRightIcon className="ml-auto h-5 w-5" />
     </Button>
   );
 }
