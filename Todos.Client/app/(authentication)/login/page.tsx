80a698e824fc8d1c10dbed9606715f62e3cd4b82
Yair Sadan <yairsadan1@gmail.com>
2024-01-23 11:36:04 +0200

    Add register link to login page

--- Todos.Client/app/(authentication)/login/page.tsx
+++ Todos.Client/app/(authentication)/login/page.tsx
@@ -1,8 +1,19 @@
 import LoginForm from '@/components/login-form';
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
+      <Link
+        href="/register"
+        className={cn(
+          buttonVariants({ variant: 'ghost' }),
+          'absolute right-4 top-4 md:right-8 md:top-8'
+        )}>
+        Register
+      </Link>
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-center justify-center rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="flex">
