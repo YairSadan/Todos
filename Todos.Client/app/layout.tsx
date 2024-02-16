0f2de62ffed9897788774df3966eb079b02b69cb
Yair Sadan <yairsadan1@gmail.com>
2024-02-16 13:45:19 +0200

    Update CSS and package dependencies

--- Todos.Client/app/layout.tsx
+++ Todos.Client/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+      <body className={cn('bg-background font-sans antialiased', fontSans.variable)}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
